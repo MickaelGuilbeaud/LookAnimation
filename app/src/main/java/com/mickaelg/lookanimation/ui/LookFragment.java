c4013bd49b0fe86f8bf7f025dc5efe30281d87fe
Mickaël Guilbeaud <mickael.guilbeaud@itelios.com>
2015-12-04 15:47:10 +0100

    - In preparation to the fling gesture detection, click events now use the Gesture Detector class.

--- app/src/main/java/com/mickaelg/lookanimation/ui/LookFragment.java
+++ app/src/main/java/com/mickaelg/lookanimation/ui/LookFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -66,6 +69,7 @@ public class LookFragment extends Fragment {
      */
     private static float pictureTranslationY = 500f;
 
+
     /**
      * Look displayed by the view.
      */
@@ -75,6 +79,10 @@ public class LookFragment extends Fragment {
      */
     @LookFragment.PictureState
     private int mCurrentPictureState = STATE_NOT_ZOOMED;
+    /**
+     * Detector to detect
+     */
+    private GestureDetectorCompat mDetector;
 
     // endregion
 
@@ -132,8 +140,20 @@ public class LookFragment extends Fragment {
             productView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
         }
 
+        // Hide the product layouts
         mLlUpperBodyProducts.setVisibility(View.GONE);
         mLlLowerBodyProducts.setVisibility(View.GONE);
+
+        // Init the gesture detector
+        mDetector = new GestureDetectorCompat(getActivity(), new LookGestureListener());
+        mIvLook.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // Send the events to our detector that will manage them
+                mDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
     }
 
     /**
@@ -147,25 +167,10 @@ public class LookFragment extends Fragment {
         // TODO
     }
 
-    @OnClick(R.id.look_iv_look_picture)
-    public void onLookPictureClicked() {
-        switch (mCurrentPictureState) {
-            case STATE_NOT_ZOOMED:
-                mCurrentPictureState = STATE_UPPER_BODY;
-                zoomInPicture();
-                slideInUpperBodyLayout();
-                break;
-            case STATE_UPPER_BODY:
-                mCurrentPictureState = STATE_NOT_ZOOMED;
-                zoomOutPicture();
-                slideOutUpperBodyLayout();
-                break;
-            case STATE_LOWER_BODY:
-                mCurrentPictureState = STATE_NOT_ZOOMED;
-                zoomOutPicture();
-                slideOutLowerBodyLayout();
-        }
-    }
+    // endregion
+
+
+    // region UI events
 
     // endregion
 
@@ -206,6 +211,49 @@ public class LookFragment extends Fragment {
 
     private void slideOutLowerBodyLayout() {
         // TODO
+    }
+
+    // endregion
+
+
+    // region LookGestureListener
+
+    private class LookGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            switch (mCurrentPictureState) {
+                case STATE_NOT_ZOOMED:
+                    mCurrentPictureState = STATE_UPPER_BODY;
+                    zoomInPicture();
+                    slideInUpperBodyLayout();
+                    break;
+                case STATE_UPPER_BODY:
+                    mCurrentPictureState = STATE_NOT_ZOOMED;
+                    zoomOutPicture();
+                    slideOutUpperBodyLayout();
+                    break;
+                case STATE_LOWER_BODY:
+                    mCurrentPictureState = STATE_NOT_ZOOMED;
+                    zoomOutPicture();
+                    slideOutLowerBodyLayout();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            // TODO
+            return true;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            // We override and set the return to true for this method because every gesture starts with a Down event so
+            // we want to listen to this event
+            return true;
+        }
+
     }
 
     // endregion
